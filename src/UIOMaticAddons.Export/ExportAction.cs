969c9d68e71f537d3e73607659697477c8b51e6e
Tim Geyssens <tim@nibble.be>
2016-10-28 15:25:37 +0200

    Make sure the ExportAction is included

---
+++ src/UIOMaticAddons.Export/ExportAction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UIOMatic.Attributes;
+
+namespace UIOMaticAddons.Export
+{
+    [UIOMaticAction("export", "Export", "~/App_Plugins/UIOMaticAddons/export.html", Icon = "icon-document-dashed-line")]
+    public class ExportAction { }
+}

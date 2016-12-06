fa032f94b1e18ca477b9491ee00ff52b81cbc67f
Tim Geyssens <tim@nibble.be>
2016-12-06 15:19:17 +0100

    Makes sure new lines don't turn the export into a mess

--- src/UIOMaticAddons.Export/Controllers/ExportController.cs
+++ src/UIOMaticAddons.Export/Controllers/ExportController.cs
@@ -1,6 +1,9 @@
 ﻿using CsvHelper;
 using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
+using System.Reflection;
 using Umbraco.Core.IO;
 using Umbraco.Web.Editors;
 
@@ -14,6 +17,7 @@ namespace UIOMaticAddons.Export.Controllers
 
             using (var textWriter = File.CreateText(IOHelper.MapPath(@"~\App_Plugins\UIOMaticAddons\Exports\" + guid + ".csv")))
             {
+                textWriter.NewLine = "\n";
                 using (var csv = new CsvWriter(textWriter))
                 {
                     var os = new UIOMatic.Services.PetaPocoObjectService();
@@ -21,9 +25,38 @@ namespace UIOMaticAddons.Export.Controllers
                     var data = os.GetAll(UIOMatic.Helper.GetUIOMaticTypeByAlias(typeAlias));
 
                     csv.Configuration.Delimiter = ";";
+                    //csv.Configuration.QuoteAllFields = true;
                     csv.Configuration.HasHeaderRecord = true;
 
-                    csv.WriteRecords(data);
+                    //csv.WriteRecords(data);
+
+                  
+                    
+
+                    foreach (var item in data)
+                    {
+                        Type myObjOriginalType = item.GetType();
+                        PropertyInfo[] myProps = myObjOriginalType.GetProperties();
+
+
+                        //var x = new ExpandoObject() as IDictionary<string, Object>;
+                        
+
+                        foreach (var prop in myProps)
+                        {
+                            //x.Add(prop.Name, prop.GetValue(item) == null ? string.Empty: prop.GetValue(item).ToString().Replace(System.Environment.NewLine," "));
+                            csv.WriteField(prop.GetValue(item) == null ? string.Empty : prop.GetValue(item).ToString().Replace(System.Environment.NewLine, " "));
+                        }
+
+
+                        //foreach (var v in (IDictionary<string, object>)item)
+                        //{
+                        //    csv.WriteField(v.Value);
+                        //}
+                        csv.NextRecord();
+                        //csv.WriteRecord(x);
+                    }
+                  
                 }
             }
            

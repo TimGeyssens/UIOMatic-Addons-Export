bcc2e74f23b30902c8369316688dfb126086bbff
David Sheiles <dinc5150@users.noreply.github.com>
2023-10-10 01:21:08 +1100

    Updates to support Umbraco V12

--- src/UIOMaticAddons.Export/App_Plugins/UIOMaticAddons/export.controller.js
+++ src/UIOMaticAddons.Export/App_Plugins/UIOMaticAddons/export.controller.js
@@ -1,8 +1,8 @@
 ﻿angular.module("umbraco").controller("uioMaticAddons.ExportController",
-    function ($scope, uioMaticExportResource) {
-        $scope.loading = true;
-        uioMaticExportResource.getExport($scope.dialogData.typeAlias).then(function (response) {
-            $scope.file = response.data;
-            $scope.loading = false;
-        });
-});
+	function ($scope, uioMaticExportResource) {
+		$scope.loading = true;
+		uioMaticExportResource.getExport($scope.model.dialogData.typeAlias).then(function (response) {
+			$scope.file = response.data;
+			$scope.loading = false;
+		});
+	});
--- src/UIOMaticAddons.Export/ServerVariableParserEvent.cs
+++ src/UIOMaticAddons.Export/ServerVariableParserEvent.cs
@@ -2,63 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
 using UIOMaticAddons.Export.Controllers;
 using Umbraco;
-using Umbraco.Core;
-using Umbraco.Core.Composing;
-using Umbraco.Core.Persistence;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Infrastructure.WebAssets;
+using Umbraco.Extensions;
 using Umbraco.Web;
-using Umbraco.Web.JavaScript;
 
-namespace UIOMaticAddons.Export
-{
-    
-        public class ServerVariableParserEventComposer : IUserComposer
-        {
-            public void Compose(Composition composition)
-            {
-                composition.Components().Append<ServerVariableParserEvent>();
-            }
-        }
+namespace UIOMaticAddons.Export {
 
-        public class ServerVariableParserEvent : IComponent
-        {
-            public ServerVariableParserEvent()
-            {
+	public class ServerVariableParserEventComposer : IComposer {
+		public void Compose(IUmbracoBuilder builder) {
+		
+			builder.AddNotificationHandler<ServerVariablesParsingNotification, ServerVariableParserEvent>();
 
-            }
+		}
+	}
 
-            public void Initialize()
-            {
-                ServerVariablesParser.Parsing += this.ServerVariablesParser_Parsing;
-            }
+	public class ServerVariableParserEvent : INotificationHandler<ServerVariablesParsingNotification> {
+		private readonly UmbracoApiControllerTypeCollection _umbracoApiControllerTypeCollection;
+		private readonly IActionContextAccessor _actionContextAccessor;
+		private readonly IUrlHelperFactory _urlFactory;
 
-            void ServerVariablesParser_Parsing(object sender, Dictionary<string, object> e)
-            {
-                if (HttpContext.Current == null) return;
-                var urlHelper = new UrlHelper(new RequestContext(new HttpContextWrapper(HttpContext.Current), new RouteData()));
+		public ServerVariableParserEvent(UmbracoApiControllerTypeCollection umbracoApiControllerTypeCollection, IActionContextAccessor actionContext, IUrlHelperFactory urlFactory) {
+			_umbracoApiControllerTypeCollection = umbracoApiControllerTypeCollection;
+			_actionContextAccessor = actionContext;
+			_urlFactory = urlFactory;
+		}
 
-                var mainDictionary = new Dictionary<string, object>
-            {
-                {
-                    "ecBaseUrl",
-                    urlHelper.GetUmbracoApiServiceBaseUrl<ExportController>(controller => controller.ToString())
-                }
-            };
+		public void Terminate() { }
 
-                if (!e.Keys.Contains("uioMaticAddons"))
-                {
-                    e.Add("uioMaticAddons", mainDictionary);
-                }
-            }
-
-            public void Terminate()
-            { }
+		public void Handle(ServerVariablesParsingNotification notification) 
+			var urlHelper = _urlFactory.GetUrlHelper(_actionContextAccessor.ActionContext);
 
 
-        }
 
-        
-    }
+			var mainDictionary = new Dictionary<string, object>
+		{
+				{
+					"ecBaseUrl",
+					urlHelper.GetUmbracoApiServiceBaseUrl<ExportController>(_umbracoApiControllerTypeCollection, controller => controller.ToString())
+				}
+			};
+
+			if (!notification.ServerVariables.ContainsKey("uioMaticAddons")) {
+				notification.ServerVariables.Add("uioMaticAddons", mainDictionary);
+			}
+		}
+	}
+
+
+}
